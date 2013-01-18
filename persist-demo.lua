d56035ccc43863cb8e22994bc6b97a32d59d31ff
Ryan Pavlik <rpavlik@iastate.edu>
2013-01-18 13:18:56 -0600

    Improve persist demo including comparison of sizes

--- persist-demo.lua
+++ persist-demo.lua
@@ -20,25 +20,29 @@ testData.data["recurse"] = testData
 -- Make it even more complicated: direct self-reference (cycle of 1)
 testData.reallyrecurse = testData
 
--- Serialize/persist to string
-local testString = p.persist(testData)
-print(testString)
-print("--------------")
+local fullSize, minSize
+do
+	-- Serialize/persist to string
+	local testString = p.persist(testData)
+	fullSize = #testString
+	print(testString)
+	print("--------------")
 
--- restore will return nil and the error if there's a problem, so this
--- works perfectly with this assert pattern.
-local restored = assert(p.restore(testString))
+	-- restore will return nil and the error if there's a problem, so this
+	-- works perfectly with this assert pattern.
+	local restored = assert(p.restore(testString))
 
--- Now print it serialized again. Not necessarily identical, due to unspecified
--- order of iteration through key-value pairs, but must be equivalent, and
--- typically is equal.
-print(p.persist(restored))
-
+	-- Now print it serialized again. Not necessarily identical, due to unspecified
+	-- order of iteration through key-value pairs, but must be equivalent, and
+	-- typically is equal.
+	print(p.persist(restored))
+end
 
 print("-------------- Now Minified:")
 -- Same thing, but minified
 do
 	local testString = p.persist(testData, {minified=true})
+	minSize = #testString
 	print(testString)
 	print("--------------")
 	local restored = assert(p.restore(testString))
@@ -46,3 +50,6 @@ do
 	-- Show non-minified output from round trip.
 	print(p.persist(restored))
 end
+print("-------------- Size of persisted data with and without minification:")
+print(minSize, fullSize)
+print(("Savings of %d bytes, or %f percent of the full size"):format(fullSize - minSize, (fullSize - minSize)/fullSize * 100))
